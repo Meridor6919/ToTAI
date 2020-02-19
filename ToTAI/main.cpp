08d5b994e6e6411df305e38bd680b0185447db45
Michał Balicki <michalbalicki@o2.pl>
2020-02-19 20:22:46 +0100

    Cleaned up main

--- ToTAI/main.cpp
+++ ToTAI/main.cpp
@@ -3,14 +3,6 @@
 int main()
 {
 	ToTAIFunctions::Start();
-	auto a = ToTAIFunctions::GetInit();
-	auto b = ToTAIFunctions::GetAllAtributes(a.second);
-	auto c = ToTAIFunctions::GetCarNames();
-	auto d = ToTAIFunctions::GetCarParams(c[0]);
-	auto e = ToTAIFunctions::GetTireNames();
-	auto f = ToTAIFunctions::GetTireParams(e[0]);
-	auto g = ToTAIFunctions::GetTour();
-	auto h = ToTAIFunctions::NewTurn(0);
-	std::cout << "DONE" << std::endl;
-	ToTAIFunctions::Exit();
+	
+	ToTAIFunctions::Exit();	
 }
